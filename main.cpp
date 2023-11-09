dad7fbfe0d5b98bfac4402dec327e948a6b22cbf
AniaTady <anialukaszczyk24@interia.pl>
2023-11-09 14:31:30 +0100

    Game beginning

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ string input = "3 HANGMAN 8 A E m n h a , g CAT 6 d o g D O G PYTHON 10 C + + J 
 istringstream sin (input);
 
 void doSingleTest();
+void beginGame(string secretWord);
+void printEmptyGallows();
+void printHiddenWord(string secretWord);
 
 int main()
 {
@@ -22,13 +25,50 @@ void doSingleTest()
 {
     string secretWord;
     sin >> secretWord;
-    cout << secretWord;
     int charsNumber;
     sin >> charsNumber;
+    beginGame(secretWord);
     for(int i = 0; i < charsNumber; i++)
     {
         char guessedChar;
         sin >> guessedChar;
     }
 
+}
+//DONE
+void beginGame(string secretWord)
+{
+    cout << "Welcome to the Hangman Game!" << endl;
+    printEmptyGallows();
+    cout << "Secret word:" << endl;
+    printHiddenWord(secretWord);
+}
+//DONE
+void printEmptyGallows()
+{
+    cout <<
+R"(+---------------------+
+|    ____________     |
+|    |/               |
+|    |                |
+|    |                |
+|    |                |
+|    |                |
+|    |                |
+|    |                |
+|    |                |
+|    |                |
+|    |                |
+|  __|__              |
++---------------------+
+)";
+}
+//TODO
+void printHiddenWord(string secretWord)
+{
+    for(int i = 0; i < secretWord.size(); i++)
+    {
+        cout << "_" << " ";
+    }
+    cout << endl;
 }
