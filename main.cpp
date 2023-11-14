3f393d32fc7de1a3a2f73f08e0d4e49b87545996
AniaTady <anialukaszczyk24@interia.pl>
2023-11-14 22:08:31 +0100

    Adding checking latin letters and already proposed letters

--- main.cpp
+++ main.cpp
@@ -10,10 +10,13 @@ void doSingleTest();
 void beginGame(string secretWord);
 void printEmptyGallows();
 void printHiddenWord(string secretWord);
-
+void printGuesses(string correctLetters);
+bool isMistake(char guessedChar, string correctLatters);
+bool isLatinLatter(char guessedChar);
+bool wasAlreadyProposed(string correctLatters, char guessedChar);
+int n = 2;
 int main()
 {
-    int n;
     sin >> n;
     for(int i = 0; i < n; i++)
     {
@@ -28,10 +31,17 @@ void doSingleTest()
     int charsNumber;
     sin >> charsNumber;
     beginGame(secretWord);
+    int mistakesLeft = 7;
+    string correctLetters = "";
     for(int i = 0; i < charsNumber; i++)
     {
+        cout << "Number of mistakes left: " << mistakesLeft << endl;
+        printGuesses(correctLetters);
+        cout << "Please guess a letter!" << endl;
         char guessedChar;
         sin >> guessedChar;
+        cout << "Your choice: " << guessedChar << endl;
+        bool isLetterWrong = isMistake(guessedChar, correctLetters);
     }
 
 }
@@ -63,7 +73,7 @@ R"(+---------------------+
 +---------------------+
 )";
 }
-//TODO
+//DONE
 void printHiddenWord(string secretWord)
 {
     for(int i = 0; i < secretWord.size(); i++)
@@ -71,4 +81,55 @@ void printHiddenWord(string secretWord)
         cout << "_" << " ";
     }
     cout << endl;
+}
+//DONE
+void printGuesses(string correctLetters)
+{
+    if("" == correctLetters)
+    {
+        cout << "Guesses: -" << endl;
+    }
+    else
+    {
+        cout << "Guesses: " << correctLetters << endl;
+    }
+}
+//TO DO
+bool isMistake(char guessedChar, string correctLatters)
+{
+    if(not isLatinLatter(guessedChar))
+    {
+        cout << "Only Latin alphabet letters" << endl;
+        return true;
+    }
+    if(wasAlreadyProposed(correctLatters, guessedChar))
+    {
+        cout << "You've already guessed that letter!" << endl;
+        return true;
+    }
+    return false;
+}
+
+//  DONE
+bool isLatinLatter(char guessedChar)
+{
+    if(guessedChar >= 'a' and guessedChar <= 'z')
+    {
+        return true;
+    }
+    if(guessedChar >= 'A' and guessedChar <= 'Z')
+    {
+        return true;
+    }
+    return false;
+}
+//TODO
+bool wasAlreadyProposed(string correctLatters, char guessedChar)
+{
+    std::size_t found = correctLatters.find(guessedChar);
+    if (found != std::string::npos)
+    {
+        return true;
+    }
+    return false;
 }
